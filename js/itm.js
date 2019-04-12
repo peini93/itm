b1b8d8cb9c87559fcecf869c1269eeff3d9eea16
peini93 <mei319319319@gmail.com>
2019-04-12 23:03:36 +0800

    修改

--- js/itm.js
+++ js/itm.js
@@ -135,22 +135,22 @@ function MENU(){
 function ACTION(num){
 	switch(num){
 		case 1:
-			$("#b01").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b01").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 		case 2:
-			$("#b02").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b02").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 		case 3:
-			$("#b03").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b03").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 		case 4:
-			$("#b04").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b04").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 		case 5:
-			$("#b05").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b05").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 		case 6:
-			$("#b06").stop().delay(800).animate({opacity: 1, paddingTop:0},800);
+			$("#b06").stop().delay(800).animate({opacity: 1, paddingTop:0},500);
 		break;
 	}
 
